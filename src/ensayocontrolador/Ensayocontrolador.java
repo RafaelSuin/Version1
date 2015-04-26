59ea369e3f2cd64e7acb9869babe159df4086276
wemdy <wendy.gabriela.zambrano@gmail.com>
2015-04-26 14:31:57 -0500

    Update Ensayocontrolador.java

--- src/ensayocontrolador/Ensayocontrolador.java
+++ src/ensayocontrolador/Ensayocontrolador.java
@@ -14,6 +14,6 @@ public class Ensayocontrolador {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("olaaaaaa");
+        System.out.print("WENDY");
     }
 }
